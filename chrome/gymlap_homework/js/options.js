439b19111e46db435394d86a7135075e754ff55d
Lukas S <SchalkLukas20@gmail.com>
2017-12-17 00:45:29 +0100

    Added manual time setting

--- chrome/gymlap_homework/js/options.js
+++ chrome/gymlap_homework/js/options.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
   var box_loadFromWebsite_id = "#box-loadFromWebsite";
   var label_loadFromWebsite_id = "#label-loadFromWebsite";
 
+  var box_manualTime_id = "#box-manualTime";
+  var label_manualTime_id = "#label-manualTime";
+
   var box_loadingTime_id = "#box-loadingTime";
   var label_loadingTime_id = "#label-loadingTime";
 
@@ -10,15 +13,20 @@ $(document).ready(function() {
   chrome.storage.sync.get({
     //Default
     loadFromWebsite: false,
+    manualTime: false,
     loadingTime: false
   }, function(storage) {
     //Updaten von den Bestandteilen
-    $(label_loadFromWebsite_id).text("Load Data from WebUntis Website - " + (storage.loadFromWebsite ? "On" : "Off"));
+    $(label_loadFromWebsite_id).text("Daten von der WebUntis Website laden - " + (storage.loadFromWebsite ? "On" : "Off"));
     $(box_loadFromWebsite_id).prop("checked", storage.loadFromWebsite);
+
+    $(label_manualTime_id).text("Manuell Zeit zum Erinnern der Schüler einstellen - " + (storage.manualTime ? "On" : "Off"));
+    $(box_manualTime_id).prop("checked", storage.manualTime);
 
     $(label_loadingTime_id).text("Simulate loading times - " + (storage.loadingTime ? "On" : "Off"));
     $(box_loadingTime_id).prop("checked", storage.loadingTime);
   });
+
 
   $(box_loadFromWebsite_id).on("change", function() {
     var value = $(box_loadFromWebsite_id).prop("checked");
@@ -27,7 +35,21 @@ $(document).ready(function() {
     chrome.storage.sync.set({
       loadFromWebsite: value
     }, function() {
-      $(label_loadFromWebsite_id).text("Load Data from WebUntis Website - " + (value ? "On" : "Off"));
+      $(label_loadFromWebsite_id).text("Daten von der WebUntis Website laden - " + (value ? "On" : "Off"));
+    });
+
+    Materialize.toast("Gespeichert!", 2000, "rounded");
+  });
+
+
+  $(box_manualTime_id).on("change", function() {
+    var value = $(box_manualTime_id).prop("checked");
+
+    //Speichern
+    chrome.storage.sync.set({
+      manualTime: value
+    }, function() {
+      $(label_manualTime_id).text("Manuell Zeit zum Erinnern der Schüler einstellen - " + (value ? "On" : "Off"));
     });
 
     Materialize.toast("Gespeichert!", 2000, "rounded");
@@ -41,7 +63,7 @@ $(document).ready(function() {
     chrome.storage.sync.set({
       loadingTime: value
     }, function() {
-      $(label_loadingTime_id).text("Simulate loading times - " + (value ? "On" : "Off"));
+      $(label_loadingTime_id).text("Ladezeiten simulieren - " + (value ? "On" : "Off"));
     });
 
     Materialize.toast("Gespeichert!", 2000, "rounded");
